750eac723fd611e5b3e59f2bb0cb71fdcd0c1a83
Willie <williewalson@gmail.com>
2018-04-25 00:41:37 -0500

    deleted a line in main.py

--- main.py
+++ main.py
@@ -7,17 +7,15 @@ app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:kittens@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-app.secret_key = 'kittens'
+app.secret_key = 'kittenz'
 
 class Blog(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     body = db.Column(db.String(500))
-    public = db.Column(db.Boolean, default=False)
-    
+        
     def __init__(self, title, body):
         self.title = title
         self.body = body
